59e2bbc3315c6b2115d49aaeecf3f1fbfff781df
Leozinnh <leonardoaf65572005@gmail.com>
2025-06-15 20:11:39 -0300

    title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <title>MSN Messenger - Autenticação</title>
+    <title>MSN - Autenticação</title>
     <base href="./">
 
     <style>
